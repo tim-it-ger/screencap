517e7f469590fdd4b4d0990d094c75f14d78f9de
Tim <50992017+tim-it-ger@users.noreply.github.com>
2022-11-02 14:58:57 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+game "gta5"
+
+author "github.com/tim-it-ger/screencap" -- All Conributors & myself <3
+
+ui_page { "index.html" }
+
+files { "index.html", "script.js" }
+
+client_scripts { "client.lua" }
