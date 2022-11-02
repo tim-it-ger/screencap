e413b8db2eaa6ebb91a4b3797af656a9c1ebffc3
Tim <50992017+tim-it-ger@users.noreply.github.com>
2022-11-02 15:03:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const screenCapnow = async () => {
   const track = stream.getVideoTracks()[0];
   const imageCapture = new ImageCapture(track);
   const bitmap = await imageCapture.grabFrame();
-  
+  const canvas = document.getElementById('fake');
+  canvas.width = bitmap.width;
+  canvas.height = bitmap.height;
+  const context = canvas.getContext('2d');
+  context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
+  const image = canvas.toDataURL('image/png');
+  const res = await fetch(image);
+  const buff = await res.arrayBuffer();
+  const file = [
+  new File([buff], `photo_${new Date()}.jpg`, {type: 'image/jpeg' })];
+  document.write('<img src="'+image+'"/>');
   track.stop();
 }
 
